fdf23956a0719a3717220095f42009f09add511a
Q-wrld97 <dough_boy97@hotmail.com>
2023-03-10 20:55:21 -0500

    added testing code for verification

--- Home/Login/js/login.js
+++ Home/Login/js/login.js
@@ -12,9 +12,6 @@ $(window).bind('load', function() {
   }, 50);
 });
 
-
-
-
 function Login() {
   // Get all our input fields
   var email = document.getElementById('email').value;
@@ -46,6 +43,7 @@ function Login() {
           })
         }
         else {
+          // if user time has not expire
           alert('please verify your email in 24 Hrs from the time of creation of your account'); // Change with pop up
           window.location.href = '../../../Dashboard/CourseInfo/html/courseForm.html';
         }
@@ -61,3 +59,53 @@ function Login() {
     alert("Wrong Username or Password");
   });
 }
+
+// test code for login and verification delete after 60 seconds instead
+/*function Login() {
+  // Get all our input fields
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  // Check authorization
+  auth.signInWithEmailAndPassword(email, password)
+  .then((res) => {
+    //store login time
+    db.collection('users').doc(res.user.uid).update({
+      timeLogin: new Date()
+    });
+    // if email is not verified
+    if (res.user.emailVerified == false) {
+      // get creationDate from firestore
+      var userdata = db.collection('users').doc(res.user.uid).get();
+      // use snapshot to query timeVerification
+      userdata.then((snapshot) => {
+        var timeVerification = snapshot.data().timeVerification;
+        // if 24 hours have passed, delete user from Auth and Firestore
+        if (new Date().getTime() - timeVerification >=  60 * 1000) {
+          // delete user from firestore
+          db.collection('users').doc(res.user.uid).delete()
+            .then(() => {
+            // delete user from auth
+            res.user.delete()
+            alert('Your account has been deleted because you did not verify your email within 24 Hrs of account creation.');
+            window.location.href = '../../Register/html/register.html';
+            //stop running code
+          })
+        }
+        else {
+          // if user time has not expire
+          alert('please verify your email in 24 Hrs from the time of creation of your account'); // Change with pop up
+          window.location.href = '../../../Dashboard/CourseInfo/html/courseForm.html';
+        }
+      });
+    }
+    else {
+      //verified email go to 
+      window.location.href = '../../../Dashboard/CourseInfo/html/courseForm.html';
+    }
+  }).catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert("Wrong Username or Password");
+  });
+}
+*/
