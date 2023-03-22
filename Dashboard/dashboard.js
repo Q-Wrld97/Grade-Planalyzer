a808315e92377457537547f2548d3489c1a6ec9b
mSakib20 <msakib2022@fau.edu>
2023-03-22 18:14:01 -0400

    Implented weather API nad chart JS for LOOKAHEAD

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -44,11 +44,6 @@ async function signOut() {
   });
 }
 
-
-
-
-
-
 /* Tabs Section */
 function openTab(evt, cityName) {
     var i, tabcontent, tablinks;
@@ -63,3 +58,112 @@ function openTab(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+//=======================This Part is for LOOK AHEAD================================//
+
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    //timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+   // dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 10000);
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        
+        let {latitude, longitude } = success.coords;
+
+        const API_KEY ='c78e07cb8ae9610397a15131d4cbbde5';
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+        console.log(data)
+        showWeatherData(data);
+        })
+
+    })
+}
+
+function showWeatherData (data){
+    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+
+
+    let otherDayForcast = ''
+    data.daily.forEach((day, idx) => {
+        if(idx == 0){
+            currentTempEl.innerHTML = `
+            <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+            <div class="other">
+                <div class="temp">${window.moment(day.dt * 1000).format('MMMM Do, YYYY')}</div>
+                <br>
+                <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+                <div class="temp">Night - ${day.temp.night}&#176;F</div>
+                <div class="temp">Day - ${day.temp.day}&#176;F</div>
+            
+            </div>
+            
+            `
+        }else{
+            otherDayForcast += `
+            <div class="weather-forecast-item">
+                <div class="temp">${window.moment(day.dt * 1000).format('MMMM Do, YYYY')}</div>
+                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <div class="temp">Night - ${day.temp.night}&#176;F</div>
+                <div class="temp">Day - ${day.temp.day}&#176;F</div>
+            </div>
+            
+            `
+        }
+    })
+
+
+    weatherForecastEl.innerHTML = otherDayForcast;
+}
+
+//============This part is for Pie-Chart================//
+
+new Chart(document.getElementById('pieChart'), {
+  type: 'pie',
+  data: {
+    labels: ["SWE", "MAT", "COT", "CEN", "CNT"],
+    datasets: [{
+      backgroundColor: ["#e63946", "#254BDD",
+        "#ffbe0b", "#1d3557", "#326998"
+      ],
+      data: [10, 60, 30, 20, 80]
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Pie Chart'
+    },
+    reponsive: true,
+    legend: {
+      position: 'bottom'
+    },
+  }
+});
+
+//===========Look Ahead Ends here==================//
