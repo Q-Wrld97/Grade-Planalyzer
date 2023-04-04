a578f07743c14b986daa5ff64f6f0c1b0961b423
mSakib20 <msakib2022@fau.edu>
2023-04-04 01:39:45 -0400

    implenting front end for courseInfo

---
+++ Dashboard/courseInfo.js
@@ -0,0 +1,9 @@
+let addButton = document.querySelector('.course-square');
+let container = document.querySelector('.courseInfoContainer');
+
+addButton.addEventListener('click', function() {
+  let newButton = document.createElement('div');
+  newButton.classList.add('course-square', 'added-course');
+  newButton.innerHTML = '<i class="fa fa-book"></i><span>Course Name</span> <button class="edit-button">Edit</button> <button class="delete-button">Delete</button>';
+  container.prepend(newButton);
+});
