c7e08f2118bea44b7e0a10eada3cd561d0124d58
mSakib20 <msakib2022@fau.edu>
2023-04-05 01:21:48 -0400

    adding delete functionality  in courseInfo

--- Dashboard/courseInfo.js
+++ Dashboard/courseInfo.js
@@ -5,5 +5,11 @@ addButton.addEventListener('click', function() {
   let newButton = document.createElement('div');
   newButton.classList.add('course-square', 'added-course');
   newButton.innerHTML = '<i class="fa fa-book"></i><span>Course Name</span> <button class="edit-button">Edit</button> <button class="delete-button">Delete</button>';
+  
+  let deleteButton = newButton.querySelector('.delete-button');
+  deleteButton.addEventListener('click', function() {
+    newButton.remove();
+  });
+  
   container.prepend(newButton);
 });
