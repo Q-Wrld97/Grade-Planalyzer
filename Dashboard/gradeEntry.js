a5e30a704ecaa2cd5b92eb067140e34868ec6ee4
Q-wlrd97 <dough_boy97@hotmail.com>
2023-04-07 12:56:06 -0400

    grade entry update

--- Dashboard/gradeEntry.js
+++ Dashboard/gradeEntry.js
@@ -1,5 +1,5 @@
-let globalData; 
-
+let globalGrades; 
+let globalComplete;
 //grabbing data from the database
 async function getGradeEntry() {
   let userID = auth.currentUser.uid;
@@ -22,6 +22,7 @@ async function getGradeEntry() {
     "participation",
   ];
   let allCategoryTypeData = {};
+  let allFinishTypeData = {};
   for (let i = 0; i < classList.length; i++) {
     let classData = {};
     for (let j = 0; j < categoryList.length; j++) {
@@ -42,7 +43,28 @@ async function getGradeEntry() {
   }
   removeUndefined(allCategoryTypeData);
   console.log(allCategoryTypeData);
-  globalData = allCategoryTypeData;
+  globalGrades = allCategoryTypeData;
+  for (let i = 0; i < classList.length; i++) {
+    let classData = {};
+    for (let j = 0; j < categoryList.length; j++) {
+      let categoryData = {};
+      let data = await db
+        .collection("users")
+        .doc(userID)
+        .collection(semester)
+        .doc(classList[i])
+        .collection(categoryList[j])
+        .doc(categoryList[j] + "Complete")
+        .get();
+      //store the data in the categoryData object
+      categoryData = data.data();
+      classData[categoryList[j]] = categoryData;
+    }
+    allFinishTypeData[classList[i]] = classData;
+  }
+  removeUndefined(allFinishTypeData);
+  console.log(allFinishTypeData);
+  globalComplete = allFinishTypeData;
   return allCategoryTypeData;
 }
 //removes undefined values from the object 
@@ -63,7 +85,7 @@ function extractKeys(obj) {
   }
   return result;
 }
-//tabbing function
+//tabbing function for grade entry tab
 function openTab(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -79,10 +101,10 @@ function openTab(evt, cityName) {
 }
 //populate the tab for the grade entry
 async function populateCourseTab() {
-  if (globalData === undefined) {
+  if (globalGrades === undefined) {
     var data = await getGradeEntry();
   } else {
-    var data = globalData;
+    var data = globalGrades;
   }
   let courseList = extractKeys(data);
   let courseTab = document.getElementById("courseTab");
@@ -96,16 +118,14 @@ async function populateCourseTab() {
       course.className = "tablinks";
       course.innerHTML = courseList[i];
       course.id = buttonId;
+      course.onclick =function() { populateGradeEntryTab(this.innerHTML) };
       courseTab.appendChild(course);
-      course.onclick = function (event) {
-        openTab(event, courseList[i]);
-        
-      };
+      
     }
   }
 }
 
-//create an event listner for id=gradeEntry
+//create an event  for id=gradeEntry
 document.getElementById("gradeEntryIcon").addEventListener("click", (e) => {
   // Call the openTab function with the appropriate parameters
   openTab(e, 'GradeEntry');
@@ -113,4 +133,48 @@ document.getElementById("gradeEntryIcon").addEventListener("click", (e) => {
   // Call the populateCourseTab function
   populateCourseTab();
 });
+
+
+// function to populate the grade entry tab
+async function populateGradeEntryTab(courseName) {
+  if (globalGrades === undefined) {
+    var data = await getGradeEntry();
+  } else {
+    var data = globalGrades;
+  }
+  if (globalComplete === undefined) {
+    var data2 = await getGradeEntry();
+  } else {
+    var data2 = globalComplete;
+  }
+  let courseData = data[courseName];
+  let courseData2 = data2[courseName];
+  console.log(courseData)
+  let gradeEntry = document.getElementById("recentFormSize");
+  gradeEntry.innerHTML = "";
+  // go throught the courseData if any field is null, populate it in html
+  for (const key in courseData) {
+    for (const key2 in courseData[key]) {
+      if (courseData[key][key2] === null && courseData2[key][key2] === true) {
+        let gradeEntryForm = document.createElement("div");
+        gradeEntryForm.className = "form-group";
+        gradeEntry.appendChild(gradeEntryForm);
+        let gradeEntryLabel = document.createElement("label");
+        gradeEntryLabel.innerHTML = key2;
+        gradeEntryLabel.className="recentFormLabel"
+        gradeEntryForm.appendChild(gradeEntryLabel);
+        let gradeEntryInput = document.createElement("input");
+        gradeEntryInput.id=key2;
+        gradeEntryInput.type=Number;
+        gradeEntryInput.className="form-control";
+        gradeEntryLabel.appendChild(gradeEntryInput)
+
+      }
+    }
+  }
+}
+
+
+
+
 
