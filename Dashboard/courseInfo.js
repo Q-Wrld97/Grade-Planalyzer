95027c60c7170c43a5663d19e980c05168cbc336
Q-wrld97 <dough_boy97@hotmail.com>
2023-04-08 16:17:30 -0400

    implemented edit and delete

--- Dashboard/courseInfo.js
+++ Dashboard/courseInfo.js
@@ -39,11 +39,64 @@ async function getCourseInfoForInfoTab() {
     deleteButton.id = classList[i];
     deleteButton.addEventListener("click", async function () {
       //alert to confirm deletion
-      const userConfirmation = confirm("Do you want to proceed with deleting?");
-      if (userConfirmation) {
+      const userConfirmationDelete = confirm(
+        "Do you want to proceed with deleting?"
+      );
+      if (userConfirmationDelete) {
         newButton.remove();
-        //delete from database
-        await db.collection("users").doc(userID).collection(semester).doc(deleteButton.id).delete();
+        //delete all subcollections first
+        let categoryList = [
+          "exam",
+          "quiz",
+          "assignment",
+          "discussion",
+          "project",
+          "participation",
+        ];
+        for (let i = 0; i < categoryList.length; i++) {
+          const subcollectionRef = await db
+            .collection("users")
+            .doc(userID)
+            .collection(semester)
+            .doc(deleteButton.id)
+            .collection(categoryList[i]);
+
+          const documentsSnapshot = await subcollectionRef.get();
+
+          // Delete documents one by one
+          for (const doc of documentsSnapshot.docs) {
+            await subcollectionRef.doc(doc.id).delete();
+          }
+        }
+        //delete the whole course
+        await db
+          .collection("users")
+          .doc(userID)
+          .collection(semester)
+          .doc(deleteButton.id)
+          .delete();
+      } else {
+        //do nothing
+      }
+    });
+    //edit button
+    let editButton = newButton.querySelector(".edit-button");
+    editButton.id = classList[i] + "edit";
+    editButton.addEventListener("click", function () {
+      const userConfirmationEdit = confirm(
+        "Do you want to proceed with editing?"
+      );
+      if (userConfirmationEdit) {
+        courseName = editButton.id.slice(0, -4);
+        semesterSeason= semester.slice(0, -4);
+        semesterYear = semester.replace(/\D/g, '');
+        console.log(semesterYear);
+        console.log(semesterSeason);
+        //pass through the course name and semester to the edit page
+        var para = new URLSearchParams();
+        para.append("courseName", courseName);
+        para.append("semesterSeason", semesterSeason);
+        para.append("semesterYear", semesterYear);
       } else {
         //do nothing
       }
@@ -51,3 +104,11 @@ async function getCourseInfoForInfoTab() {
     container.prepend(newButton);
   }
 }
+
+//event listener for the course info tab
+document.getElementById("courseInfoIcon").addEventListener("click", (e) => {
+  // Call the openTab function with the appropriate parameters
+  openTab(event, "CourseInfo");
+  // Call the populateCourseTab function
+  getCourseInfoForInfoTab();
+});
