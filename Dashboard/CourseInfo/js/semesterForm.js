6d0c8b400a1b5a0070a27dd470e20e78d5d931ee
mSakib20 <msakib2022@fau.edu>
2023-04-23 15:22:18 -0400

    added functionality to grb vlues frm the smstr frm

--- Dashboard/CourseInfo/js/semesterForm.js
+++ Dashboard/CourseInfo/js/semesterForm.js
@@ -11,3 +11,19 @@ $(window).bind('load', function () {
         opacity -= opacity * 0.1;
     }, 50);
 });
+
+document.getElementById('next-btn').addEventListener('click', function () {
+    // Get the values from the form
+    let semesterSeason = document.getElementById('semester-season').value;
+    let semesterYear = document.getElementById('semester-year').value;
+    let semesterStartingDate = document.getElementById('semester-starting-date').value;
+    let semesterEndingDate = document.getElementById('semester-ending-date').value;
+
+    // Log the values to the console
+    console.log('Semester Season:', semesterSeason);
+    console.log('Semester Year:', semesterYear);
+    console.log('Semester Starting Date:', semesterStartingDate);
+    console.log('Semester Ending Date:', semesterEndingDate);
+
+    // You can add code here to submit the form or perform other actions with the form data
+});
