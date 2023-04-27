465cf2821cb3d29dfeed981bd7ea3ebbdcd365ce
mSakib20 <msakib2022@fau.edu>
2023-04-26 21:39:14 -0400

    Nicholas's report issue and FAQ

---
+++ Dashboard/FAQ/js/faq.js
@@ -0,0 +1,6 @@
+$('.toggle-response').on('click', function() {
+    $(this).toggleClass('open');
+    $(this).next('.response').slideToggle();
+  });
+
+ 
---
+++ Dashboard/ReportIssue/js/reportIssue.js
@@ -0,0 +1,48 @@
+$('.toggle-response').on('click', function() {
+    $(this).toggleClass('open');
+    $(this).next('.response').slideToggle();
+  });
+
+  var firebaseConfig = {
+    // Your Firebase configuration goes here
+  };
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+
+  var form = document.getElementById("form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var formData = new FormData(form);
+    var name = formData.get("name");
+    var category = formData.get("category");
+    var date = formData.get("date");
+    var time = formData.get("time");
+    var email = formData.get("email");
+    var comment = formData.get("comment");
+    var file = formData.get("file");
+    var storageRef = firebase.storage().ref();
+    var fileRef = storageRef.child(file.name);
+    fileRef.put(file).then(function(snapshot) {
+      console.log("File uploaded successfully");
+      fileRef.getDownloadURL().then(function(url) {
+        db.collection("users").add({
+          name: name,
+          category: category,
+          date: date,
+          time: time,
+          email: email,
+          comment: comment,
+          fileUrl: url
+        })
+
+        .then(function(docRef) {
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+          console.error("Error adding document: ", error);
+        });
+      });
+    }).catch(function(error) {
+      console.error("Error uploading file: ", error);
+    });
+  });
