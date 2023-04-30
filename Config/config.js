98a4b200c6b0498ba782b261135ce10e2d9d78ec
Q-wrld97 <dough_boy97@hotmail.com>
2023-04-30 12:29:07 -0400

    google analytic testing

--- Config/config.js
+++ Config/config.js
@@ -13,3 +13,4 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth()
 const database = firebase.database()
 const db = firebase.firestore()
+const analytics = firebase.getAnalytics();
